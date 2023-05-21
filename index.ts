aff111ab1109db7703dd5a3ff0e5eaa6504e1641
ilon-trask <misha007zyt@gmail.com>
2023-05-21 23:18:11 +0300

    changed telegraf to node-telegram-bot-api

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import { Telegraf } from "telegraf";
+import TelegramBot from "node-telegram-bot-api";
 import axios from "axios";
-import { setIntervalAsync } from "set-interval-async/fixed";
 
-const bot = new Telegraf("5932488137:AAE1PT5wJjfRG0BQ_rlz4iHzW_2ikgt3-HY");
-
+const botToken = "5932488137:AAE1PT5wJjfRG0BQ_rlz4iHzW_2ikgt3-HY";
+const bot = new TelegramBot(botToken, { polling: true });
+let chatIds: number[] = [];
 const BINANCE_API =
   "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
 const BYBIT_API = "https://api.bybit.com/v2/public/tickers?symbol=BTCUSD";
-// const OKEX_API = "https://www.okex.com/api/spot/v3/instruments/BTC-USDT/ticker";
+// const OKEX_API = 'https://www.okex.com/api/spot/v3/instruments/BTC-USDT/ticker';
 const COINBASE_API = "https://api.coinbase.com/v2/prices/BTC-USD/spot";
 
 const getPrices = async () => {
@@ -31,25 +31,47 @@ const checkPriceDifference = async () => {
   const diff = (maxPrice - minPrice) / maxPrice;
 
   if (diff >= 0.01) {
-    bot.telegram.sendMessage(
-      651494797,
-      `Price difference is more than 1%: ${JSON.stringify(prices)}`
-    );
-    // bot.telegram.reply(
-    //   `Price difference is more than 1%: ${JSON.stringify(prices)}`
-    // );
-  } else {
-    bot.telegram.sendMessage(
-      "651494797",
-      `Price difference is within 1%: ${JSON.stringify(prices)}`
-    );
+    chatIds.forEach((el) => {
+      bot.sendMessage(
+        el,
+        `Price difference is more than 1%: ${JSON.stringify(prices)}`
+      );
+    });
   }
 };
-
-// setIntervalAsync(checkPriceDifference, 10000); // перевіряємо ціни кожну хвилину
-
-bot.launch();
-// bot.telegram.sendMessage("651494797", `Price difference is within 1%: }`);
+bot.setMyCommands([
+  { command: "/start", description: "Розпочати роботу" },
+  { command: "/stop", description: "Завершити роботу" },
+  { command: "/check", description: "Перевірити дані" },
+]);
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  chatIds.push(chatId);
+  bot.sendSticker(
+    chatId,
+    "https://tlgrm.ru/_/stickers/ade/277/ade277af-adcd-3b1e-a5f8-3aab12f35748/1.webp"
+  );
+  bot.sendMessage(chatId, "Починаю працювати");
+});
+bot.onText(/\/stop/, (msg) => {
+  const chatId = msg.chat.id;
+  chatIds = chatIds.filter((el) => el != chatId);
+  bot.sendSticker(
+    chatId,
+    "https://tlgrm.ru/_/stickers/ade/277/ade277af-adcd-3b1e-a5f8-3aab12f35748/3.webp"
+  );
+  bot.sendMessage(chatId, "Ну я відпочивати");
+});
+bot.onText(/\/check/, async (msg) => {
+  const chatId = msg.chat.id;
+  const prices = await getPrices();
+  const maxPrice = Math.max(...Object.values(prices));
+  const minPrice = Math.min(...Object.values(prices));
+  bot.sendMessage(
+    chatId,
+    `Найнижча ціна ${minPrice}, найвища ціна ${maxPrice}`
+  );
+});
 
 while (true) {
   checkPriceDifference();
